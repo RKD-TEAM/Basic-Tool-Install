78be2814f56fd50d2d6c23240e42a3807d10924c
RKD-TEAM <rakibulislam.6297.r@gmail.com>
2025-08-10 22:00:20 +0600

    Update tool.py

--- tool.py
+++ tool.py
@@ -5,12 +5,13 @@
 import os #for Clear
 import sys#for Animation
 import time#for Time
+import shutil
 
 os.system("clear")
 
 #Type With Animation
 print("\n\n\n\n\n")
-ab="                 \033[33m System Loading..........."
+ab="                 \033[36m System Loading..........."
 
 for c in ab:
     sys.stdout.write(c)
@@ -27,10 +28,16 @@ for c in ab:
     sys.stdout.write(c)
     sys.stdout.flush()
     time.sleep(0.1)#Animation with Time
-Name=input("     \n\n\n            \033[1;31m Enter Your Name: ")
+
+# Name Input
+while True:
+    Name=input("     \n\n\n            \033[1;36m Enter Your Name: ").strip()
+    if Name:
+        break
+    else:
+        print("\033[91m      Name can't be empty. Please enter your name.\033[0m")
   
-  
-ab="             \033[1;32 Hey "+ Name +", Be Ethical...."
+ab="\033[1;32m             Hey "+ Name +", Be Ethical....\033[0m"
 
 for c in ab:
     sys.stdout.write(c)
@@ -40,20 +47,33 @@ print("\n\n\n")
 time.sleep(2)
 os.system("clear")
 
-print("""
-      :::::::::  :::    ::: ::::::::: 
-     :+:    :+: :+:   :+:  :+:    :+: 
-    +:+    +:+ +:+  +:+   +:+    +:+  
-   +#++:++#:  +#++:++    +#+    +:+   
-  +#+    +#+ +#+  +#+   +#+    +#+    
- #+#    #+# #+#   #+#  #+#    #+#     
-###    ### ###    ### #########        
-\n\n \033[1;32m============================                                                             \n Owner : RKD TEAM
- Github : Something
- Facebook :RKD TEAM
-==============================
-\033[0m""") 
+# Terminal width
+columns = shutil.get_terminal_size().columns
 
+# RKD logo
+logo_lines = [
+    ":::::::::  :::    ::: ::::::::: ",
+    ":+:    :+: :+:   :+:  :+:    :+:",
+    "+:+    +:+ +:+  +:+   +:+    +:+",
+    "+#++:++#:  +#++:++    +#+    +:+",
+    "+#+    +#+ +#+  +#+   +#+    +#+",
+    "#+#    #+# #+#   #+#  #+#    #+#",
+    "###    ### ###    ### #########"
+]
+
+print("\033[1;91m")
+for line in logo_lines:
+    print(line.center(columns))
+print("\033[0m")
+
+print("\033[32m" + "=" * columns + "\033[1;96m")
+print(" Owner     : Mr.Rakibul Islam")
+print(" Github    : https://github.com/RKD-TEAM")
+print(" Facebook  : Alex Rk Khan")
+print(" Tool Name : _____________")
+print("\033[32m" + "=" * columns + "\033[1;96m")
+print("\033[31m!!!   This tool is for educational purposes only   !!!!!!   So don't use it for any illegal activities   !!!")
+print("\033[32m" + "=" * columns + "\033[0m")
 
 
 #Basic Package Installer Tool
